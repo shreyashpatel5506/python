a9cd25ce9bc89611b3f3edb4c713696f4ad56a48
shreyash patel <160259841+shreyashpatel5506@users.noreply.github.com>
2024-08-27 08:43:42 +0000

    time

--- w86.py
+++ w86.py
@@ -2,3 +2,8 @@ x=30
 y=10
 sum=int(x)+int(y)
 print("{}+{}={}".format(x,y,sum))
+# Define two variables, x and y, and assign values 30 and 20, respectively.
+x = 30
+y = 20
+# Print the sum of x and y using string formatting.
+print("\n%d+%d=%d" % (x, y, x+y))
---
+++ w90.py
@@ -0,0 +1,8 @@
+import time
+
+current_time = time.strftime("%H:%M:%S")
+print("Current time:", current_time)
+from datetime import datetime
+
+current_time = datetime.now().time()
+print("Current time:", current_time)
