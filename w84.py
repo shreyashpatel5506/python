d668b8c40e83bd0b1de39f58972311e5a4fd8dd8
shreyash patel <160259841+shreyashpatel5506@users.noreply.github.com>
2024-08-27 07:52:27 +0000

    chactre find

--- w84.py
+++ w84.py
@@ -1,1 +1,8 @@
-py
+str=input("enter a string::::")
+charcter=input("entef a charcter which you want to count")
+count=int(0)
+for i in range(len(str)) :
+    if charcter==str[i] :
+        count+=1
+        
+print(count)
